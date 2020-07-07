1d35fdab8b5818f3be1fa0a83447203f0d9fb9e0
Ikram Gabiyev <ikram20011127@gmail.com>
2020-07-07 17:27:20 +0400

    added colors file and set up fonts

--- App.js
+++ App.js
@@ -1,21 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+//standard imports
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//importing AppLoading
+//currently for uploading files in the render() in [App.js]
+import { AppLoading } from 'expo';
+
+//importing font package
+import * as Font from 'expo-font';
+
+/**
+ * fetches all fonts;
+ * returns a promise;
+ * put as a prop into AppLoading component
+ */
+const fetchFonts = () => {
+    return Font.loadAsync({
+        //use this mostly
+        'mont-alt-regular': require('./assets/fonts/MontserratAlternates-Regular.ttf'),
+
+        //use this for subTitles or when regular is too thin
+        'mont-alt-medium': require('./assets/fonts/MontserratAlternates-Medium.ttf'),
+
+        //use this for titles
+        'mont-alt-bold': require('./assets/fonts/MontserratAlternates-Bold.ttf'),
+    });
+};
+
+/**
+ * Here we will have the main SwitchNavigator rendered
+ */
+class App extends Component 
+{
+    state = {
+        //checks if fonts loaded
+        fontsLoaded: false,
+    };
+
+    render() 
+    {
+        /**
+         * render AppLoading screen if 
+         * fonts are still not loaded
+         */
+        if (!this.state.fontsLoaded)
+        {
+            return (
+                <AppLoading
+                    startAsync={fetchFonts}
+                    onFinish={() => this.setState({ fontsLoaded: true })}
+                    onError={console.warn}
+                />
+            );
+        }
+
+		return (
+			<View style={styles.container}>
+				<Text>Hey App.js is here</Text>
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
+
+export default App;
---
+++ constants/colors.js
@@ -0,0 +1,30 @@
+/**
+ * Please use these colors ONLY, in the app;
+ * even if you are setting colors to plain white or black
+ */
+
+export default {
+
+    //this is the color of the logo
+    logoColor: '#545371',
+
+    //this is the color of the logo label
+    //it is brighter than logoColor and has a transparent feel to it
+    logoLabelColor: '#aaa8b8',
+
+    //color with high saturation (and a bit dark)
+    //can be good for backgrounds of the screens
+    screenColor: '#441299',
+
+    //color for placeHolders (e.g. in TextInputs)
+    //it is a dark grey color
+    placeHolderColor: '#737275',
+
+    //currently black color
+    //can be mainly used for basic texts
+    customBlack: '#000000',
+
+    //currently white color
+    //use cases: general
+    customWhite: '#ffffff',
+};
