841bbebea04a89894b1117a7dc21dd9593bd5918
Ikram Gabiyev <ikram20011127@gmail.com>
2020-07-07 20:03:59 +0400

    updated expo sdk and solved issues with navigation

--- App.js
+++ App.js
@@ -3,32 +3,32 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 //importing the entry navigator
-//import EntryNavigator from './navigation/EntryNavigator';
+import EntryNavigator from './navigation/EntryNavigator';
 
 //importing AppLoading
 //currently for uploading files in the render() in [App.js]
 import { AppLoading } from 'expo';
 
 //importing font package
-//import * as Font from 'expo-font';
+import * as Font from 'expo-font';
 
 /**
  * fetches all fonts;
  * returns a promise;
  * put as a prop into AppLoading component
  */
-// const fetchFonts = () => {
-//     return Font.loadAsync({
-//         //use this mostly
-//         'mont-alt-regular': require('./assets/fonts/MontserratAlternates-Regular.ttf'),
+const fetchFonts = () => {
+    return Font.loadAsync({
+        //use this mostly
+        'mont-alt-regular': require('./assets/fonts/MontserratAlternates-Regular.ttf'),
 
-//         //use this for subTitles or when regular is too thin
-//         'mont-alt-medium': require('./assets/fonts/MontserratAlternates-Medium.ttf'),
+        //use this for subTitles or when regular is too thin
+        'mont-alt-medium': require('./assets/fonts/MontserratAlternates-Medium.ttf'),
 
-//         //use this for titles
-//         'mont-alt-bold': require('./assets/fonts/MontserratAlternates-Bold.ttf'),
-//     });
-// };
+        //use this for titles
+        'mont-alt-bold': require('./assets/fonts/MontserratAlternates-Bold.ttf'),
+    });
+};
 
 /**
  * Here we will have the main SwitchNavigator rendered
@@ -46,21 +46,19 @@ class App extends Component
          * render AppLoading screen if 
          * fonts are still not loaded
          */
-        // if (!this.state.fontsLoaded)
-        // {
-        //     return (
-        //         <AppLoading
-        //             startAsync={fetchFonts}
-        //             onFinish={() => this.setState({ fontsLoaded: true })}
-        //             onError={console.warn}
-        //         />
-        //     );
-        // }
+        if (!this.state.fontsLoaded)
+        {
+            return (
+                <AppLoading
+                    startAsync={fetchFonts}
+                    onFinish={() => this.setState({ fontsLoaded: true })}
+                    onError={console.warn}
+                />
+            );
+        }
 
 		return (
-			<View style={styles.container}>
-				<Text>Hey App.js is here</Text>
-			</View>
+			<EntryNavigator />
 		);
 	}
 }
