b674262ee39297d764f425f3153f4e2bfbfee587
kgobakis <kostas004@hotmail.com>
2020-07-08 17:06:03 +0300

    changed background color

--- screens/Authentication/SignIn/SignInScreen.js
+++ screens/Authentication/SignIn/SignInScreen.js
@@ -1,26 +1,28 @@
-import React, { Component } from 'react';
-import { ScrollView, Text, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import { ScrollView, Text, StyleSheet } from "react-native";
+import Colors from "../../../constants/colors";
 
 class SignInScreen extends Component {
-	static navigationOptions = {
-		title: 'Sign In',
-	};
+  static navigationOptions = {
+    title: "Sign In",
+  };
 
-	render() {
-		return (
-			<ScrollView contentContainerStyle={styles.screen}>
-				<Text>Hey</Text>
-			</ScrollView>
-		);
-	}
+  render() {
+    return (
+      <ScrollView contentContainerStyle={styles.screen}>
+        <Text>a</Text>
+      </ScrollView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	screen: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
+  screen: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: Colors.logoColor,
+  },
 });
 
 export default SignInScreen;
