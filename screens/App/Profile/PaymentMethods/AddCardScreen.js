f2ce0b1743daf3a6b6285692d71263e54ff4483b
Ikram Gabiyev <ikram20011127@gmail.com>
2020-07-12 21:18:33 +0400

    added the AddCardScreen with a credit card animation

--- screens/App/Profile/PaymentMethods/AddCardScreen.js
+++ screens/App/Profile/PaymentMethods/AddCardScreen.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+//outside imports
+import { CreditCardInput } from 'react-native-credit-card-input';
+
+//constants
+import Colors from '../../../../constants/colors';
+
+//components
+import CustomScrollView from '../../../../components/CustomScrollView';
+
 class AddCardScreen extends Component
 {
+    _onChange = form => console.log(form);
+
     render() {
+        
+
         return (
-            <View>
-                <Text>Add a Card Here</Text>
-            </View>
+            <CustomScrollView backgroundColor={Colors.backgroundGrey}>
+                <CreditCardInput onChange={this._onChange} requiresName={true}/>
+            </CustomScrollView>
         );
     }
 }
