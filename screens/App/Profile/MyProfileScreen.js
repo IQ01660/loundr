7ee49b68f0ee8669364021df11ec90451357a6d2
Ikram Gabiyev <ikram20011127@gmail.com>
2020-07-12 15:51:35 +0400

    done with picking the profile photo and storing it in documentDirectory

--- screens/App/Profile/MyProfileScreen.js
+++ screens/App/Profile/MyProfileScreen.js
@@ -3,37 +3,95 @@ import { StyleSheet, View, Alert } from 'react-native';
 
 //outside imports
 import * as ImagePicker from 'expo-image-picker';
+import * as FileSystem from 'expo-file-system';
 
 //components
 import CustomScrollView from '../../../components/CustomScrollView';
 import MinorButton from '../../../components/MinorButton';
+import Avatar from '../../../components/Avatar';
 
 //constants
 import Colors from '../../../constants/colors';
 import FontSizes from '../../../constants/fontSizes';
-
-const openImagePickerAsync = async () => {
-    const permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
-    
-    if (permissionResult.granted === false) {
-      Alert.alert("Access denied", "You can grant access through settings later");
-      return;
-    }
-
-    const pickerResult = await ImagePicker.launchImageLibraryAsync();
-    console.log(pickerResult);
-};
+const AVATAR_WIDTH_HEIGHT = 80; // the profile photo's side length (for borderRadius and uri photos)
 
 class MyProfileScreen extends Component {
+	state = {
+        //
+		avatarSource: require('../../../assets/avatar.png'),
+	};
+
+    /**
+     * handling the click "Pick a photo"
+     */
+    openImagePickerAsync = async () => {
+        //asking for permission if haven't asked already
+        const permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+    
+        //if permission not granted - stop the function
+        if (permissionResult.granted === false) {
+            Alert.alert('Access denied', 'You can grant access through settings later');
+            return;
+        }
+    
+        //open the Camera Roll and store user's choice
+        const pickerResult = await ImagePicker.launchImageLibraryAsync();
+        
+        //if the user did not pick anything - stop the function
+        if (pickerResult.cancelled === true) {
+            return;
+        }
+        
+        //if the user picked an image
+        //store its initial uri - position in the cache
+        //to get the name of the file
+        const fileName = pickerResult.uri.split('/').pop();
+
+        //create a path with that file name in the document directory
+        //which is a permanent local memry of the app on the phone
+        const permanentFilePath = FileSystem.documentDirectory + fileName;
+
+        //if no errors move the image from cache to permanent storage
+        try {
+            await FileSystem.moveAsync({
+                from: pickerResult.uri,
+                to: permanentFilePath,
+            })
+
+            //update the state and the DB (later)
+            this.setState({
+                avatarSource: {
+                    uri: permanentFilePath,
+                },
+            });
+
+            //also send it to DB
+
+        } 
+        //if you got an error log it and use the avatar image from assets
+        catch(err) {
+            console.log(err);
+            this.setState({
+                avatarSource: require('../../../assets/avatar.png'),
+            });
+        }
+    };
+
 	render() {
 		return (
 			<CustomScrollView backgroundColor={Colors.customWhite} style={styles.container}>
-				<View style={styles.photoPickerContainer} >
+                {/* container for the photo picking stuff */}
+				<View style={styles.photoPickerContainer}>
+                    {/* the avatar container */}
+					<View style={styles.imageContainer}>
+						<Avatar source={this.state.avatarSource} width={AVATAR_WIDTH_HEIGHT} />
+					</View>
+                    {/* "pick the photo" button */}
 					<MinorButton
 						color={Colors.logoColor}
 						style={styles.pickPhotoBtn}
 						title="Pick a photo"
-						onPress={openImagePickerAsync}
+						onPress={this.openImagePickerAsync}
 					/>
 				</View>
 			</CustomScrollView>
@@ -43,9 +101,14 @@ class MyProfileScreen extends Component {
 
 const styles = StyleSheet.create({
 	container: {},
-    photoPickerContainer: {
-        alignItems: 'center',
-    },
+	photoPickerContainer: {
+		alignItems: 'center',
+	},
+	imageContainer: {
+		width: AVATAR_WIDTH_HEIGHT,
+		height: AVATAR_WIDTH_HEIGHT,
+		marginVertical: 30,
+	},
 	pickPhotoBtn: {},
 });
 
