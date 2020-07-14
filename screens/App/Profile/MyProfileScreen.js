ed5e5b18346fea9326e558aa549c319a9da36103
Ikram Gabiyev <ikram20011127@gmail.com>
2020-07-14 19:38:53 +0400

    connecting to aws s3

--- screens/App/Profile/MyProfileScreen.js
+++ screens/App/Profile/MyProfileScreen.js
@@ -4,6 +4,17 @@ import { StyleSheet, View, Alert } from 'react-native';
 //outside imports
 import * as ImagePicker from 'expo-image-picker';
 import * as FileSystem from 'expo-file-system';
+import { RNS3 } from 'react-native-aws3';
+
+const options = {
+	keyPrefix: 'uploads/',
+	bucket: 'loundr-images',
+	region: 'us-east-2',
+	accessKey: 'AKIAJURMZG7RVDDU2MFA',
+	secretKey: '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',
+	successActionStatus: 201,
+};
+
 
 //components
 import CustomScrollView from '../../../components/CustomScrollView';
@@ -14,81 +25,90 @@ import OptionsButton from '../../../components/OptionsButton';
 //constants
 import Colors from '../../../constants/colors';
 import FontSizes from '../../../constants/fontSizes';
-const DEFAULT_AVATAR_PATH = '../../../assets/avatar.jpg'
+const DEFAULT_AVATAR_PATH = '../../../assets/avatar.jpg';
 const AVATAR_WIDTH_HEIGHT = 80; // the profile photo's side length (for borderRadius and uri photos)
-const CENTRAL_PANEL_WIDTH = '100%'
+const CENTRAL_PANEL_WIDTH = '100%';
 
 class MyProfileScreen extends Component {
 	state = {
 		avatarSource: require(DEFAULT_AVATAR_PATH),
 	};
 
-    /**
-     * handling the click "Pick a photo"
-     */
-    openImagePickerAsync = async () => {
-        //asking for permission if haven't asked already
-        const permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
-    
-        //if permission not granted - stop the function
-        if (permissionResult.granted === false) {
-            Alert.alert('Access denied', 'You can grant access through settings later');
-            return;
-        }
-    
-        //open the Camera Roll and store user's choice
-        const pickerResult = await ImagePicker.launchImageLibraryAsync();
-        
-        //if the user did not pick anything - stop the function
-        if (pickerResult.cancelled === true) {
-            return;
-        }
-        
-        //if the user picked an image
-        //store its initial uri - position in the cache
-        //to get the name of the file
-        const fileName = pickerResult.uri.split('/').pop();
+	/**
+	 * handling the click "Pick a photo"
+	 */
+	openImagePickerAsync = async () => {
+		//asking for permission if haven't asked already
+		const permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
 
-        //create a path with that file name in the document directory
-        //which is a permanent local memry of the app on the phone
-        const permanentFilePath = FileSystem.documentDirectory + fileName;
+		//if permission not granted - stop the function
+		if (permissionResult.granted === false) {
+			Alert.alert('Access denied', 'You can grant access through settings later');
+			return;
+		}
 
-        //if no errors move the image from cache to permanent storage
-        try {
-            await FileSystem.moveAsync({
-                from: pickerResult.uri,
-                to: permanentFilePath,
-            })
+		//open the Camera Roll and store user's choice
+		const pickerResult = await ImagePicker.launchImageLibraryAsync();
 
-            //update the state and the DB (later)
-            this.setState({
-                avatarSource: {
-                    uri: permanentFilePath,
-                },
+		//if the user did not pick anything - stop the function
+		if (pickerResult.cancelled === true) {
+			return;
+		}
+
+		//if the user picked an image
+		//store its initial uri - position in the cache
+		//to get the name of the file
+		const fileName = pickerResult.uri.split('/').pop();
+
+		//create a path with that file name in the document directory
+		//which is a permanent local memry of the app on the phone
+		const permanentFilePath = FileSystem.documentDirectory + fileName;
+
+		//if no errors move the image from cache to permanent storage
+		try {
+			await FileSystem.moveAsync({
+				from: pickerResult.uri,
+				to: permanentFilePath,
             });
 
-            //also send it to DB
+            console.log(permanentFilePath);
+            
+            const file = {
+                // uri can also be a file system path (i.e. file://)
+                uri: permanentFilePath,
+                name: "ikram-profile-0.png",
+                type: "image/png"
+            }
 
-        } 
-        //if you got an error log it and use the avatar image from assets
-        catch(err) {
-            console.log(err);
-            this.setState({
-                avatarSource: require(DEFAULT_AVATAR_PATH),
-            });
+			//update the state and the DB (later)
+			this.setState({
+				avatarSource: {
+					uri: permanentFilePath,
+				},
+			});
+
+			//also send it to DB
+		} catch (err) {
+			//if you got an error log it and use the avatar image from assets
+			console.log(err);
+			this.setState({
+				avatarSource: require(DEFAULT_AVATAR_PATH),
+			});
         }
-    };
+        
+        
+	};
 
 	render() {
 		return (
 			<CustomScrollView backgroundColor={Colors.backgroundGrey} style={styles.container}>
-                {/* container for the photo picking stuff */}
+				{/* container for the photo picking stuff */}
 				<View style={styles.photoPickerContainer}>
-                    {/* the avatar container */}
+					{/* the avatar container */}
 					<View style={styles.imageContainer}>
 						<Avatar source={this.state.avatarSource} width={AVATAR_WIDTH_HEIGHT} />
 					</View>
-                    {/* "pick the photo" button */}
+					{/* "pick the photo" button */}
 					<MinorButton
 						color={Colors.logoColor}
 						style={styles.pickPhotoBtn}
@@ -96,41 +116,45 @@ class MyProfileScreen extends Component {
 						onPress={this.openImagePickerAsync}
 					/>
 				</View>
-                <View style={styles.options}>
-                    <OptionsButton iconName="credit-card" title="Payment Methods" onPress={() => {
-                        this.props.navigation.navigate('PaymentMethods')
-                    }}/>
-                    <OptionsButton iconName="lock" title="Privacy" />
-                    <OptionsButton iconName="user" title="Edit Profile" />
-                    <OptionsButton iconName="sign-out" title="Sign Out" isLast />
-                </View>
-        	</CustomScrollView>
+				<View style={styles.options}>
+					<OptionsButton
+						iconName="credit-card"
+						title="Payment Methods"
+						onPress={() => {
+							this.props.navigation.navigate('PaymentMethods');
+						}}
+					/>
+					<OptionsButton iconName="lock" title="Privacy" />
+					<OptionsButton iconName="user" title="Edit Profile" />
+					<OptionsButton iconName="sign-out" title="Sign Out" isLast />
+				</View>
+			</CustomScrollView>
 		);
 	}
 }
 
 const styles = StyleSheet.create({
 	container: {
-        alignItems: 'center',
-    },
+		alignItems: 'center',
+	},
 	photoPickerContainer: {
-        width: CENTRAL_PANEL_WIDTH,
-        alignItems: 'center',
+		width: CENTRAL_PANEL_WIDTH,
+		alignItems: 'center',
 	},
 	imageContainer: {
 		width: AVATAR_WIDTH_HEIGHT,
 		height: AVATAR_WIDTH_HEIGHT,
 		marginVertical: 30,
 	},
-    options: {
-        width: CENTRAL_PANEL_WIDTH,
-        backgroundColor: Colors.customWhite,
-        marginVertical: 30,
-        borderBottomWidth: 1,
-        borderBottomColor: Colors.placeHolderColor,
-        borderTopWidth: 1,
-        borderTopColor: Colors.placeHolderColor,
-    }
+	options: {
+		width: CENTRAL_PANEL_WIDTH,
+		backgroundColor: Colors.customWhite,
+		marginVertical: 30,
+		borderBottomWidth: 1,
+		borderBottomColor: Colors.placeHolderColor,
+		borderTopWidth: 1,
+		borderTopColor: Colors.placeHolderColor,
+	},
 });
 
 export default MyProfileScreen;
