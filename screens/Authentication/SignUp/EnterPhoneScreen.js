da23baf3cde003fc2d738cb04e8f46631d941add
Ikram Gabiyev <ikram20011127@gmail.com>
2020-07-16 13:06:30 +0400

    setting up phone verification

--- screens/Authentication/SignUp/EnterPhoneScreen.js
+++ screens/Authentication/SignUp/EnterPhoneScreen.js
@@ -1,17 +1,50 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+//components
+import CustomScrollView from '../../../components/CustomScrollView';
+
+//constants
+import Colors from '../../../constants/colors';
+
+//outside imports
+import PhoneInput from 'react-native-phone-input'
+
+import { FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha';
+import firebaseConfig from '../../../Firebase'
+
 class EnterPhoneScreen extends Component
 {
+    constructor(props)
+    {
+        super(props);
+        this.recaptchaRef = React.createRef();
+    }
+
     render() {
         return (
-            <View>
-                <Text>Enter Phone Screen!!</Text>
-            </View>
+            <CustomScrollView backgroundColor={Colors.logoColor} style={styles.container} >
+                <View style={styles.inner} >
+                    
+                    <FirebaseRecaptchaVerifierModal 
+                        ref={this.recaptchaRef}
+                        firebaseConfig={firebaseConfig}
+                    />
+                </View>
+            </CustomScrollView>
         );
     }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center'
+    },
+    inner: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
 
 export default EnterPhoneScreen;
