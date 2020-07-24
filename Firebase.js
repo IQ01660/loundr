9e8a7e4daf6076daff0408457487fefe321fae8b
Ikram Gabiyev <ikram20011127@gmail.com>
2020-07-24 20:27:22 +0400

    added cors config for the firebase storage access

--- Firebase.js
+++ Firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
 	authDomain: 'loundr-fced9.firebaseapp.com',
 	databaseURL: 'https://loundr-fced9.firebaseio.com',
 	projectId: 'loundr-fced9',
-	storageBucket: 'loundr-fced9.appspot.com',
+	storageBucket: 'gs://loundr-fced9.appspot.com',
 	messagingSenderId: '28905429703',
 	appId: '1:28905429703:web:1cc5160bd2b907759d3ce2',
 	measurementId: 'G-BMV4TW6QFQ',
--- screens/Authentication/SignIn/ForgotPassScreen.js
+++ screens/Authentication/SignIn/ForgotPassScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 
 import CustomScrollView from '../../../components/CustomScrollView';
 import ErrorMessage from '../../../components/Auth/ErrorMessage';
