7ca5dfcdbd0389e688acf2877812e3e067198c87
Ikram Gabiyev <ikram20011127@gmail.com>
2020-08-02 15:31:08 +0400

    added all banks' info and input fields

--- constants/banksInfo.js
+++ constants/banksInfo.js
@@ -7,22 +7,33 @@ export const BanksList = [
     { label: 'Bank VTB', value: 'bank-vtb' },
 	{ label: 'Bank Eurasia', value: 'bank-eurasia' },
     { label: 'Bank of Baku', value: 'bank-of-baku' },
-    { label: 'Bank Respublica', value: 'bank-respublica' },
+    { label: 'Bank Respublika', value: 'bank-respublika' },
     { label: 'Pasha Bank', value: 'pasha-bank' },
     { label: 'Premium Bank', value: 'premium-bank' },
     { label: 'MuganBank', value: 'mugan-bank' },
 ];
 
 //requirements
-const PIN_CODE = "Pin Code (from Identity Card): Last 7 characters excluding the last one"
-const DoB = "Date of Birth: DD.MM. YYYY";
-const CLIENT_CODE = "Client Code";
+const PIN_CODE = "Pin Code (from Identity Card/Şəxsiyyət vəsiqəsi): Last 7 characters excluding the last one"
+const DoB = "Date of Birth (Doğum tarixi): DD.MM.YYYY";
+const CLIENT_CODE = "Client Code (Müştəri kodu)";
 const CLIENT_ID = "Client ID: PAŞA Bankda hesab açmış müştəriyə bank tərəfindən təqdim edilmiş indentifikasiya nömrəsi nəzərdə tutulur";
-const ID_CARD_NUMBER = "ID Card Number: e.g. AZEXXXXXXXX"
+const ID_CARD_NUMBER = "ID Card Number: e.g. AZEXXXXXXXX";
+const ACCOUNT_NUMBER = "Account Number (Hesab nömrəsi)"
+
 
 
 //the requirements of all banks by their code names
 export const BanksInputs = {
     "premium-bank": [PIN_CODE, DoB],
     "pasha-bank": [CLIENT_ID, ID_CARD_NUMBER],
+    "bank-vtb": [],
+    "mugan-bank": [],
+    "bank-of-baku": [CLIENT_CODE, DoB],
+    "access-bank": [ID_CARD_NUMBER, CLIENT_CODE],
+    "afb-bank": [ACCOUNT_NUMBER, DoB],
+    "azer-turk-bank": [],
+    "bank-btb": [CLIENT_CODE, DoB],
+    "bank-eurasia": ["Ş/V seriya nömrəsi (e.g. 09531177)", DoB],
+    "bank-respublika": [ACCOUNT_NUMBER, ID_CARD_NUMBER],
 };
